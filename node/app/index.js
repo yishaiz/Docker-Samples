995863a1090f198a61cbf441f1ab71c7c3efb71e
yishai <yishai5555@gmail.com>
2020-06-03 19:10:51 +0300

    01-add-node-app

---
+++ node/app/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hi there');
+});
+
+app.listen(8080, () => {
+  console.log('Server is listenning n port 8080');
+});
