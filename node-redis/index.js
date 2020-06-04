8ce11ae480b5a210b3e6139745542619aa2a4a6f
yishai <yishai5555@gmail.com>
2020-06-04 20:27:44 +0300

    03-node-with-redis-docker-compose

--- node-redis/index.js
+++ node-redis/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+});
 
 const port = 8081;
 client.set('visits', 0);
